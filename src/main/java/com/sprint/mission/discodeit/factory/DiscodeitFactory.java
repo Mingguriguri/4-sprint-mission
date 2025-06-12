7434b35b8f08236b823abe564e9cdf32271dafed
Minjeong Kim <rlaalswjd-@naver.com>
2025-06-12 11:58:34 +0900

    feat: factory 디자인 패턴 구현 방식을 생성자 함수 방식으로 변경 및 개선 - JCF기반 구현체와 FILE 기반 구현체를 ENUM으로 분기처리

--- src/main/java/com/sprint/mission/discodeit/factory/DiscodeitFactory.java
+++ src/main/java/com/sprint/mission/discodeit/factory/DiscodeitFactory.java
@@ -1,27 +1,50 @@
 package com.sprint.mission.discodeit.factory;
 
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.file.FileChannelService;
 import com.sprint.mission.discodeit.service.file.FileMessageService;
 import com.sprint.mission.discodeit.service.file.FileUserService;
+import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
+import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
+import com.sprint.mission.discodeit.service.jcf.JCFUserService;
 
+// 이 부분은 팩토리 패턴을 일부 구현하기 위해서 만든 클래스입니다
 public class DiscodeitFactory {
-    // JCF 기반 구현체로 실제 비즈니스 로직이 처리되는 Service 인스턴스
-    UserService userService = new FileUserService();
-    ChannelService channelService = new FileChannelService();
-    MessageService messageService = new FileMessageService(userService, channelService);
+    private final PersistenceType type;
+
+    public DiscodeitFactory(PersistenceType type) {
+        this.type = type;
+    }
 
     public UserService getUserService() {
-        return userService;
+        if (type == PersistenceType.JCF) {
+            return new JCFUserService(new JCFUserRepository(), new JCFMessageRepository());
+        } else {
+            return new FileUserService(new FileUserRepository(), new FileMessageRepository());
+        }
     }
 
     public ChannelService getChannelService() {
-        return channelService;
+        if (type == PersistenceType.JCF) {
+            return new JCFChannelService(new JCFChannelRepository(), new JCFMessageRepository());
+        } else {
+            return new FileChannelService(new FileChannelRepository(), new FileMessageRepository());
+        }
     }
 
     public MessageService getMessageService() {
-        return messageService;
+        if (type == PersistenceType.JCF) {
+            return new JCFMessageService(new JCFMessageRepository());
+        } else {
+            return new FileMessageService(new FileMessageRepository());
+        }
     }
 }
