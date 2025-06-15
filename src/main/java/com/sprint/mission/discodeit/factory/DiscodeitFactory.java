e917ebac4d1d5af03981d04ecb932ed2677faef9
Minjeong Kim <rlaalswjd-@naver.com>
2025-06-15 20:46:12 +0900

    refactor: factory 클래스에 있는 JCF*Service나 File*Service를 Basic*Service로 대체

--- src/main/java/com/sprint/mission/discodeit/factory/DiscodeitFactory.java
+++ src/main/java/com/sprint/mission/discodeit/factory/DiscodeitFactory.java
@@ -9,6 +9,9 @@ import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import com.sprint.mission.discodeit.service.file.FileChannelService;
 import com.sprint.mission.discodeit.service.file.FileMessageService;
 import com.sprint.mission.discodeit.service.file.FileUserService;
@@ -17,6 +20,7 @@ import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
 import com.sprint.mission.discodeit.service.jcf.JCFUserService;
 
 // 이 부분은 팩토리 패턴을 일부 구현하기 위해서 만든 클래스입니다
+// JCFUser
 public class DiscodeitFactory {
     private final PersistenceType type;
 
@@ -24,27 +28,42 @@ public class DiscodeitFactory {
         this.type = type;
     }
 
-    public UserService getUserService() {
-        if (type == PersistenceType.JCF) {
-            return new JCFUserService(new JCFUserRepository(), new JCFMessageRepository());
-        } else {
-            return new FileUserService(new FileUserRepository(), new FileMessageRepository());
+    public UserService createUserService() {
+        switch (type) {
+            case JCF:
+                // JCFUserService -> BasicUserService로 교체
+                return new BasicUserService(new JCFUserRepository(), new JCFMessageRepository());
+            case FILE:
+                // FileUserService -> BasicUserService로 교체
+                return new BasicUserService(new FileUserRepository(), new FileMessageRepository());
+            default:
+                return null;
         }
     }
 
-    public ChannelService getChannelService() {
-        if (type == PersistenceType.JCF) {
-            return new JCFChannelService(new JCFChannelRepository(), new JCFMessageRepository());
-        } else {
-            return new FileChannelService(new FileChannelRepository(), new FileMessageRepository());
+    public ChannelService createChannelService() {
+        switch (type) {
+            case JCF:
+                // JCFChannelService -> BasicChannelService로 교체
+                return new BasicChannelService(new JCFChannelRepository(), new JCFMessageRepository());
+            case FILE:
+                // FileChannelService -> BasicChannelService로 교체
+                return new BasicChannelService(new FileChannelRepository(), new FileMessageRepository());
+            default:
+                return null;
         }
     }
 
-    public MessageService getMessageService() {
-        if (type == PersistenceType.JCF) {
-            return new JCFMessageService(new JCFMessageRepository());
-        } else {
-            return new FileMessageService(new FileMessageRepository());
+    public MessageService creteMessageService() {
+        switch (type) {
+            case JCF:
+                // JCFMessageService -> BasicMessageService
+                return new BasicMessageService(new JCFMessageRepository());
+            case FILE:
+                // FileMessageService -> BasicMessageService
+                return new BasicMessageService(new FileMessageRepository());
+            default:
+                return null;
         }
     }
 }
