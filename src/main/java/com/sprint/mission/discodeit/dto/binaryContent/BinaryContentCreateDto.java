7180f4e034f3e27466fb0b0ecc6a5761d07c7a7b
Minjeong Kim <rlaalswjd-@naver.com>
2025-07-10 10:21:26 +0900

    fix: BinaryContent를 생성할 경우, 기본생성자가 없어서 생성이 되지 않는 문제 해결 - @RequiredArgsConstructor와 final을 제거하고 @Setter 와 @NoArgsConstructor로 대체

--- src/main/java/com/sprint/mission/discodeit/dto/binaryContent/BinaryContentCreateDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/binaryContent/BinaryContentCreateDto.java
@@ -3,17 +3,17 @@ package com.sprint.mission.discodeit.dto.binaryContent;
 import com.sprint.mission.discodeit.entity.BinaryContentType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@RequiredArgsConstructor
+@Setter
+@NoArgsConstructor
 public class BinaryContentCreateDto {
     @Schema(description = "바이너리 컨텐츠 타입", example = "PROFILE")
     @NotNull
-    private final BinaryContentType type;
+    private BinaryContentType type;
 
     @Schema(description = "바이너리 파일")
-    private final MultipartFile file;
+    private MultipartFile file;
 }
