1c0ede3cec74e85b986d6efae10d2a5e8c4b386b
Minjeong Kim <rlaalswjd-@naver.com>
2025-07-22 10:45:08 +0900

    feat(database): Spring Data JPA 적용 및 데이터베이스 초기 설정 추가 - 데이터베이스 DDL 파일 추가 - Spring Data JPA와 PostgreSQL을 위한 의존성을 추가 - application.yaml에 데이터베이스 연결 설정, SQL 로그 설정 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,84 @@
+CREATE TABLE binary_contents (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    size BIGINT NOT NULL,
+    content_type VARCHAR(100) NOT NULL,
+    bytes BYTEA NOT NULL
+);
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+    profile_id UUID
+        REFERENCES binary_contents(id)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE user_statuses(
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    user_id UUID NOT NULL UNIQUE
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+    last_active_at TIMESTAMP NOT NULL
+);
+
+
+CREATE TYPE channel_type AS ENUM ('PUBLIC', 'PRIVATE');
+
+CREATE TABLE channels (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    name VARCHAR(100),
+    description VARCHAR(500),
+    type channel_type NOT NULL
+);
+
+CREATE TABLE read_statuses (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    user_id UUID NOT NULL
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+    channel_id UUID NOT NULL
+        REFERENCES channels(id)
+        ON DELETE CASCADE,
+    last_read_at TIMESTAMP NOT NULL,
+    -- 복합 유니크
+    CONSTRAINT uk_read_statuses_user_channel
+    UNIQUE(user_id, channel_id)
+);
+
+CREATE TABLE messages (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    content TEXT,
+    channel_id UUID NOT NULL
+        REFERENCES channels(id)
+        ON DELETE CASCADE,
+    author_id UUID
+        REFERENCES users(id)
+        ON DELETE SET NULL
+);
+
+
+CREATE TABLE message_attachments (
+ message_id UUID
+     REFERENCES messages(id)
+     ON DELETE CASCADE,
+ attachment_id UUID
+     REFERENCES binary_contents(id)
+     ON DELETE CASCADE,
+
+ CONSTRAINT pk_message_attachments
+     PRIMARY KEY (message_id, attachment_id)
+);
